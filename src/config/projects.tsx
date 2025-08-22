62f58625930d9ebff6c5effbf0bf6dee9b23b16a
YaPo <113506317+0xYaPo@users.noreply.github.com>
2025-08-22 11:36:40 +0200

    Addition Project X staking

--- src/config/projects.tsx
+++ src/config/projects.tsx
@@ -1916,4 +1916,11 @@ export const projects: ProjectType[] = [
         url: 'https://senseitemple.com/',
         icon: '/logos/SenseiEcosystem.png',
     },
+ {
+        category: Category.staking,
+        name: 'Project X Staking',
+        url: 'https://explorer.multiversx.com/identities/ProjectX',
+        icon: '/logos/project-x.png',
+    },
+
 ];
